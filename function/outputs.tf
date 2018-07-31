7eca77c890e265c51712f32f063c46dd9e333cf5
Aaron Collier <aaron.collier@stanford.edu>
2018-07-31 09:31:33 -0700

    Add vars

--- function/outputs.tf
+++ function/outputs.tf
@@ -5,3 +5,11 @@ output arn {
 output id {
   value = "${aws_lambda_function.lambda_function.id}"
 }
+
+output name {
+  value = "${aws_lambda_function.lambda_function.name}"
+}
+
+output invoke_arn {
+  value = "${aws_lambda_function.lambda_function.invoke_arn}"
+}
