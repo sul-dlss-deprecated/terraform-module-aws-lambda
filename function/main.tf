88efb83e8fae3846605d0c60a4377758572c4dd8
Aaron Collier <aaron.collier@stanford.edu>
2018-09-07 14:26:24 -0700

    Update naming of lambda function

--- function/main.tf
+++ function/main.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "lambda_function" {
   handler       = "${var.handler}"
-  function_name = "${var.function_name}"
+  function_name = "${var.function_name}-${var.environment}"
   memory_size   = "${var.memory_size}"
   role          = "${var.execution_role_arn}"
   runtime       = "${var.runtime}"
   s3_bucket     = "${var.s3_bucket}"
-  s3_key        = "${var.function_name}/lambda.zip"
+  s3_key        = "${var.function_name}-${var.environment}/lambda.zip"
   timeout       = "${var.timeout}"
 
   vpc_config {
