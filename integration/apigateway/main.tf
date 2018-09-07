959a1dd48b95242e2c32d2e8046e8432c7e7b2ae
Aaron Collier <aaron.collier@stanford.edu>
2018-09-07 15:03:03 -0700

    Fix typo

--- integration/apigateway/main.tf
+++ integration/apigateway/main.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 resource "aws_api_gateway_resource" "rest_api_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   parent_id   = "${aws_api_gateway_rest_api.rest_api.root_resource_id}"
-  path_part   = "${var.function.name}"
+  path_part   = "${var.function_name}"
 }
 
 resource "aws_api_gateway_method" "rest_api_method" {
