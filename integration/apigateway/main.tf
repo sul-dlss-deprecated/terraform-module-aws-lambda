5382d114cd715218f7d9cdc043f3aae6d6ceae86
Aaron Collier <aaron.collier@stanford.edu>
2018-10-22 16:39:06 -0700

    Fix typo'

--- integration/apigateway/main.tf
+++ integration/apigateway/main.tf
@@ -97,5 +97,5 @@ resource "aws_api_gateway_api_key" "api_key" {
 resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   key_id        = "${aws_api_gateway_api_key.api_key.id}"
   key_type      = "API_KEY"
-  usage_plan_id = "${aws_api_gateway_usage_plan.rialto_sparql_loader_usage_plan.id}"
+  usage_plan_id = "${aws_api_gateway_usage_plan.api_usage_plan.id}"
 }
