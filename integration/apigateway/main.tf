924316e8f8ef4aa42ceae557c095242421477550
Aaron Collier <aaron.collier@stanford.edu>
2018-10-22 16:37:57 -0700

    Add depends_on to ensure order

--- integration/apigateway/main.tf
+++ integration/apigateway/main.tf
@@ -87,6 +87,7 @@ resource "aws_api_gateway_usage_plan" "api_usage_plan" {
     stage  = "${var.environment}"
   }
 
+  depends_on = ["aws_api_gateway_deployment.rest_api_deployment"]
 }
 
 resource "aws_api_gateway_api_key" "api_key" {
@@ -96,5 +97,5 @@ resource "aws_api_gateway_api_key" "api_key" {
 resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   key_id        = "${aws_api_gateway_api_key.api_key.id}"
   key_type      = "API_KEY"
-  usage_plan_id = "${aws_api_gateway_usage_plan.api_usage_plan.id}"
+  usage_plan_id = "${aws_api_gateway_usage_plan.rialto_sparql_loader_usage_plan.id}"
 }
