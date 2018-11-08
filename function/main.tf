240c55484a799cba243e2812bea3ab6237fea1a0
Aaron Collier <aaron.collier@stanford.edu>
2018-11-08 14:02:40 -0800

    Calculate a source hash based on the s3 udpated timestamp

--- function/main.tf
+++ function/main.tf
@@ -1,11 +1,17 @@
+data "aws_s3_bucket_object" "lambda" {
+  bucket = "${var.s3_bucket}"
+  key    = "${var.function_name}-${var.environment}/lambda.zip"
+}
+
 resource "aws_lambda_function" "lambda_function" {
   handler                        = "${var.handler}"
   function_name                  = "${var.function_name}-${var.environment}"
   memory_size                    = "${var.memory_size}"
   role                           = "${var.execution_role_arn}"
   runtime                        = "${var.runtime}"
-  s3_bucket                      = "${var.s3_bucket}"
-  s3_key                         = "${var.function_name}-${var.environment}/lambda.zip"
+  s3_bucket                      = "${data.aws_s3_bucket_object.lambda.bucket}"
+  s3_key                         = "${data.aws_s3_bucket_object.lambda.key}"
+  source_code_hash               = "${base64sha256("${data.aws_s3_bucket_object.lambda.last_modified}")}"
   timeout                        = "${var.timeout}"
   reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
 
